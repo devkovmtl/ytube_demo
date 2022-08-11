4eef01097303664ca993d6e2f305f22407c355c3
Mikael <dev.kov.mtl@gmail.com>
2022-08-11 10:56:23 -0400

    Server - Connect to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
 dotenv.config();
 
-const { PORT } = process.env;
+const { PORT, MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_DATABASE } =
+  process.env;
 
 const app = express();
+
+const connect = () => {
+  // Setup mongoose db
+  const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DATABASE}?retryWrites=true&w=majority`;
+  const MONGO_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
+  mongoose
+    .connect(MONGO_URL, MONGO_OPTIONS)
+    .then(() => {
+      console.log("Connect to DB");
+    })
+    .catch((err) => {
+      console.log("Error");
+    });
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,4 +37,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.listen(PORT, () => console.log("Server started"));
+app.listen(PORT, () => {
+  connect();
+  console.log("Server started");
+});
