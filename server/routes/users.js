21b0dd947fc35030fcbb31dac0b613ff5708513b
Mikael <dev.kov.mtl@gmail.com>
2022-08-15 17:53:32 -0400

    Server - user routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,35 @@
 import express from "express";
-import { test } from "../controllers/user.js";
+import {
+  udpate,
+  deleteUser,
+  getUser,
+  subscribe,
+  unsubscribe,
+  like,
+  dislike,
+} from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get("/test", test);
+// update user
+router.put("/:id", udpate);
+
+// delete user
+router.delete("/:id", deleteUser);
+
+// get user
+router.get("/find/:id", getUser);
+
+// subscribe user
+router.put("/sub/:id", subscribe);
+
+// unsubscribe user
+router.put("/sub/:id", unsubscribe);
+
+// like video
+router.put("/like/:videoId", like);
+
+// dislike a video
+router.put("/dislike/:videoId", dislike);
 
 export default router;
