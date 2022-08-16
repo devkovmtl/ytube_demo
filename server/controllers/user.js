eba94126c14a945e032c54ddcf618a6c70165e51
Mikael <dev.kov.mtl@gmail.com>
2022-08-16 15:07:32 -0400

    Server - user controller delete update get user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,13 +1,68 @@
-export const udpate = (req, res, next) => {};
+import { createError } from "../error.js";
+import User from "../models/User.js";
 
-export const deleteUser = (req, res, next) => {};
+export const udpate = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updateUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updateUser);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(createError(403, "You can update only your account!"));
+  }
+};
 
-export const getUser = (req, res, next) => {};
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
 
-export const subscribe = (req, res, next) => {};
+      res.status(200).json("User has been deleted.");
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(createError(403, "You can delete only your account!"));
+  }
+};
 
-export const unsubscribe = (req, res, next) => {};
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const like = (req, res, next) => {};
+export const subscribe = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+  } else {
+  }
+};
 
-export const dislike = (req, res, next) => {};
+export const unsubscribe = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+  } else {
+  }
+};
+
+export const like = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+  } else {
+  }
+};
+
+export const dislike = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+  } else {
+  }
+};
