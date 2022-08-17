dae8676d71d85554d10f1aa7156b66318c920a1f
Mikael <dev.kov.mtl@gmail.com>
2022-08-17 16:55:50 -0400

    Server - updated video routes

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -4,6 +4,10 @@ import {
   updateVideo,
   getVideo,
   deleteVideo,
+  addView,
+  trendVideo,
+  randomVideo,
+  subVideo,
 } from "../controllers/video.js";
 import { verifyToken } from "../verifyToken.js";
 
@@ -11,11 +15,19 @@ const router = express.Router();
 
 // create a video
 router.post("/", verifyToken, addVideo);
-
+// update a video
 router.put("/:id", verifyToken, updateVideo);
-
+// delete a video
 router.delete("/:id", verifyToken, deleteVideo);
-
+// get single video
 router.get("/find/:id", getVideo);
+
+router.put("/view/:id", addView);
+
+router.get("/trend", trendVideo);
+
+router.get("/random", randomVideo);
+
+router.get("/sub", subVideo);
 
 export default router;
