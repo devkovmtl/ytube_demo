dc5bfcb9ec3c0c9ef8d4c0f7e4531f8dcfd46fb3
Mikael <dev.kov.mtl@gmail.com>
2022-08-18 15:22:05 -0400

    Server - Fixed bug delete comment

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -16,7 +16,7 @@ export const deleteComment = async (req, res, next) => {
   try {
     const comment = await Comment.findById(req.params.id);
     const video = await Video.findById(comment.videoId);
-    if (req.user.id === commment.userId || req.user.id === video.userId) {
+    if (req.user.id === comment.userId || req.user.id === video.userId) {
       await Comment.findByIdAndDelete(req.params.id);
       res.status(200).json("The comment has been deleted.");
     } else {
