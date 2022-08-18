1eca067785d9f7a95c923a274175d1f7086aceda
Mikael <dev.kov.mtl@gmail.com>
2022-08-18 14:04:27 -0400

    Server - Comments delete comment from video

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -1,3 +1,4 @@
+import { createError } from "../error.js";
 import Comment from "../models/Comment.js";
 import Video from "../models/Video.js";
 
@@ -7,6 +8,21 @@ export const addComment = async (req, res, next) => {
 
     const savedComment = await newComment.save();
     res.status(200).send(savedComment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comment.findById(req.params.id);
+    const video = await Video.findById(comment.videoId);
+    if (req.user.id === commment.userId || req.user.id === video.userId) {
+      await Comment.findByIdAndDelete(req.params.id);
+      res.status(200).json("The comment has been deleted.");
+    } else {
+      return next(createError(403, "You can delete only your comment."));
+    }
   } catch (error) {
     next(error);
   }
