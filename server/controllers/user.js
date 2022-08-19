9f164d4e8055b9c291fbc26d4c576d3d84305eb8
Mikael <dev.kov.mtl@gmail.com>
2022-08-19 10:09:35 -0400

    Server - like and dislike video controller

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -72,13 +72,35 @@ export const unsubscribe = async (req, res, next) => {
 };
 
 export const like = async (req, res, next) => {
-  if (req.params.id === req.user.id) {
-  } else {
+  const id = req.user.id;
+  const videoId = req.user.videoId;
+
+  try {
+    // make sure only once $addToSet (not added if already in)
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { likes: id },
+      $pull: { dislikes: id },
+    });
+
+    res.status(200).json("The video has been liked.");
+  } catch (err) {
+    next(err);
   }
 };
 
 export const dislike = async (req, res, next) => {
-  if (req.params.id === req.user.id) {
-  } else {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
+
+  try {
+    // make sure only once $addToSet (not added if already in)
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { dislikes: id },
+      $pull: { likes: id },
+    });
+
+    res.status(200).json("The video has been disliked.");
+  } catch (err) {
+    next(err);
   }
 };
