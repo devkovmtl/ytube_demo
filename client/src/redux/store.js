9f0e5082452b2dcf0d0b6aa95577e83bb28bdfe5
Mikael <dev.kov.mtl@gmail.com>
2022-08-23 14:37:34 -0400

    Client - update store with videoslice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 import userReducer from "./userSlice";
+import videoReducer from "./videoSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    video: videoReducer,
   },
 });
