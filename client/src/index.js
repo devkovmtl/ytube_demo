52912ca031737b05a788c09f42a1351ea315a031
Mikael <dev.kov.mtl@gmail.com>
2022-08-24 10:31:10 -0400

    Client - redux update using persistore

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
 import App from "./App";
-import { store } from "./redux/store";
+import { store, persistore } from "./redux/store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistore} loading={null}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
